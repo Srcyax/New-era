425ac6c44bbf93bb85216aa7907021d57b261a75
Srcyax <95943570+Srcyax@users.noreply.github.com>
2023-08-29 12:23:49 -0300

    footsteps

---
+++ Assets/Player/Scripts/PlayerFootsteps.cs
@@ -0,0 +1,33 @@
+using Mirror;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    [Header("Player components")]
+    [SerializeField] PlayerComponents components;
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] walkSounds;
+    [SerializeField] AudioClip[] runSounds;
+
+    private void Start() {
+        audioSource.volume = components.localPlayer ? 0.05f : 0.8f;
+    }
+
+    public void WalkFootStep() {
+        if ( audioSource.isPlaying )
+            return;
+
+        int i = Random.Range(0, walkSounds.Length);
+
+        audioSource.clip = walkSounds[ i ];
+        audioSource.Play();
+    }
+
+    public void RunFootStep() {
+        int i = Random.Range(0, runSounds.Length);
+
+        audioSource.clip = runSounds[ i ];
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+}
