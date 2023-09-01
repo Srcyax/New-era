51c8f53923cbe48d17af32d9b18f02154a5a1992
Srcyax <95943570+Srcyax@users.noreply.github.com>
2023-09-01 17:58:06 -0300

    a

--- Assets/Network/UIManager.cs
+++ Assets/Network/UIManager.cs
@@ -3,8 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
-{
+public class UIManager : MonoBehaviour {
     [SerializeField] PlayerData playerData;
     [SerializeField] JsonSaveSystem jsonSystem;
 
@@ -25,53 +24,26 @@ public class UIManager : MonoBehaviour
     [SerializeField] Slider sensibility;
     [SerializeField] TMP_Dropdown graphics;
 
-    private void Start()
-    {
+    private void Start() {
         jsonSystem.SettingsDataLoadFromJson(sensibility, graphics, playerName);
 
         QualitySettings.SetQualityLevel(graphics.value);
 
-        HostButton?.onClick.AddListener( () => {
+        HostButton?.onClick.AddListener(() => {
             if ( ( maxClient.value + 1 ) > 1 ) {
-                try {
-                    NetworkManager.singleton.StartHost();
-                    NetworkManager.singleton.OnStartHost();
-                    transport.maxConnections = maxClient.value;
-                }
-                catch {
-                    ConnectionStatus("error creating server.");
-                }
+                NetworkManager.singleton.StartHost();
+                transport.maxConnections = maxClient.value;
             }
-        } );
+        });
 
-        ClientButton?.onClick.AddListener( () => {
-            try {
-                NetworkManager.singleton.StartClient();
-                NetworkManager.singleton.authenticator.OnStartClient();
-            } 
-            catch {
-                ConnectionStatus("connection error.");
-            }   
-        } );
-    }
-
-    void ConnectionStatus(string status) {
-        if ( connectionTransform.childCount > 0 ) {
-            for ( int i = 0; i < connectionTransform.childCount; i++ ) {
-                Destroy(connectionTransform.GetChild(i).gameObject);
-            }
-            GameObject conn = Instantiate(connectionStatus, connectionTransform);
-            conn.GetComponent<TextMeshProUGUI>().text = status;
-        }
-        else {
-            GameObject conn = Instantiate(connectionStatus, connectionTransform);
-            conn.GetComponent<TextMeshProUGUI>().text = status;
-        }
+        ClientButton?.onClick.AddListener(() => {
+            NetworkManager.singleton.StartClient();
+        });
     }
 
     private void Update() {
         playerData.name = playerName.text;
-        HostButton.interactable =  ipAdress.text.Length > 1 && playerName.text.Length > 1;
+        HostButton.interactable = ipAdress.text.Length > 1 && playerName.text.Length > 1;
         ClientButton.interactable = ipAdress.text.Length > 1 && playerName.text.Length > 1;
     }
 }
