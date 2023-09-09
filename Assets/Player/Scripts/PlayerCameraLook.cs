e34b6241414a925b1adb397d72423b68d50bd479
Srcyax <95943570+Srcyax@users.noreply.github.com>
2023-09-09 16:26:07 -0300

    Update PlayerCameraLook.cs

--- Assets/Player/Scripts/PlayerCameraLook.cs
+++ Assets/Player/Scripts/PlayerCameraLook.cs
@@ -14,6 +14,7 @@ public class PlayerCameraLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 delta = mainController.inputActions.Player.Look.ReadValue<Vector2>();
+        
+
     }
 }
